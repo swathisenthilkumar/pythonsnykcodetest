0be9a689ad0842200b6650bce4a3bd93a8e74979
swathisenthilkumar <86238993+swathisenthilkumar@users.noreply.github.com>
2023-08-08 18:48:00 +0530

    Create script.sh

---
+++ script.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+echo "Hello, World!"
+SNYK_TOKEN="$1"
+curl -Lo ./snyk "https://github.com/snyk/snyk/releases/download/v1.1060.0/snyk-linux"
+        chmod +x snyk
+        ls -lrt
+        echo "----------Authenticating Snyk-----------"
+         ./snyk auth $SNYK_TOKEN
+        echo "----------Snyk code Scanning-----------"
+         ./snyk code test
+         
+        
