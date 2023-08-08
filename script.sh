0afcde2811997486d26425fe6d247cdd22339f65
swathisenthilkumar <86238993+swathisenthilkumar@users.noreply.github.com>
2023-08-08 19:22:45 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "Hello, World!"
 SNYK_TOKEN="$1"
-curl -Lo ./snyk "https://github.com/snyk/snyk/releases/download/v1.1060.0/snyk-linux"
+curl -Lo ./snyk "https://github.com/snyk/snyk/releases/download/v1.1200.0/snyk-linux"
         chmod +x snyk
         ls -lrt
         echo "----------Authenticating Snyk-----------"
