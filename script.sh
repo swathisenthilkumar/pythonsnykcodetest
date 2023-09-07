bb19d6edf63840d292ad9ca810af3bdc240878f0
swathisenthilkumar <86238993+swathisenthilkumar@users.noreply.github.com>
2023-09-07 15:39:42 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -8,4 +8,5 @@ curl -Lo ./snyk "https://github.com/snyk/snyk/releases/download/v1.1200.0/snyk-l
          ./snyk auth $SNYK_TOKEN
         echo "----------Snyk code Scanning-----------"
          ./snyk code test --report --project-name="snykcodepoc"
+         
         
