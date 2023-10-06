a50277b987c315e1d7336f95e49c032477cf798e
swathisenthilkumar <86238993+swathisenthilkumar@users.noreply.github.com>
2023-10-06 17:10:24 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -6,6 +6,8 @@ curl -Lo ./snyk "https://github.com/snyk/snyk/releases/download/v1.1200.0/snyk-l
         ls -lrt
         echo "----------Authenticating Snyk-----------"
          ./snyk auth $SNYK_TOKEN
+         echo "install snyk-filter"
+         npm i -g snyk-filter
         echo "----------Snyk code Scanning-----------"
          ./snyk code test --report --project-name="snykcodepoc"
          
